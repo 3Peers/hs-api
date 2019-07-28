979ec22ddfb71fa339a7b724a6b07c0bc32b6e8a
Sachin Kukreja <skad5455@gmail.com>
2019-07-28 12:18:20 +0530

    Added script to generate coverage report

---
+++ run_test_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./manage.py test -v 2 --noinput\
+                      --with-coverage\
+                      --cover-package='user,entities,assessments,problems'\
+                      --cover-branches\
+                      --cover-inclusive\
+                      --cover-erase "$@"
