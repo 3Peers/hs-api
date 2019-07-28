91a0908249de84a4e2a4b8e3c7169ef255d8bcfd
Saket <saket.is.sam@gmail.com>
2019-07-28 15:48:12 +0530

    Added /user/me/ endpoint to return current user.

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import UserListCreateView, UserRetrieveDeleteView, SendOTPView, VerifyOTPView
-
+from .views import (
+    UserListCreateView,
+    UserRetrieveDeleteView,
+    GetCurrentUserView,
+    SendOTPView,
+    VerifyOTPView
+)
 
 urlpatterns = [
     path('', UserListCreateView.as_view(), name='list_and_create'),
+    path('me/', GetCurrentUserView.as_view(), name='get_current_user'),
     path('sign-up/send-otp/', SendOTPView.as_view(), name='send_otp_view'),
     path('sign-up/verify-otp/', VerifyOTPView.as_view(), name='verify_otp_view'),
     path('<int:pk>/', UserRetrieveDeleteView.as_view(), name='get_and_delete')
