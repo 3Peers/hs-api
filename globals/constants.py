b4dd367cf9dc114b49528b8830a4b6ba868065a5
Saket <saket.is.sam@gmail.com>
2019-08-04 20:01:44 +0530

    Added change password endpoint

--- globals/constants.py
+++ globals/constants.py
@@ -1,3 +1,6 @@
 class ResponseMessages:
     INVALID_EMAIL = 'Invalid Email'
     BAD_REQUEST = 'Bad Request'
+
+
+MIN_PASSWORD_LENGTH = 8
---
+++ user/tests/test_password.py
@@ -0,0 +1,91 @@
+from django.shortcuts import reverse
+from rest_framework.test import APITestCase
+from ..models import User
+from ..views import (
+    BAD_PASSWORD_PROVIDED,
+    RESET_PASSWORD_SUCCESS,
+    UNMATCHING_PASSWORDS
+)
+
+
+class ChangePasswordViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='username',
+            email='email@email.com',
+            password='random passwd'
+        )
+
+        self.url = reverse('change_password')
+
+    def test_unauthenticated_request(self):
+        """Should not allow unauthorized access /user/change-password/
+        """
+        data = {
+            'password': '123',
+            'repeat_password': '123'
+        }
+        response = self.client.post(self.url, data=data)
+
+        expected_response_code = 401
+        self.assertEqual(response.status_code, expected_response_code)
+
+    def test_bad_password(self):
+        """Should be a bad request if bad password provided \
+        (not following strong password practices) at /user/change-password/
+        """
+
+        data = {
+            'password': '123',
+            'repeat_password': '123'
+        }
+
+        # TODO: Use context manager
+        self.client.force_authenticate(self.user)
+        response = self.client.post(self.url, data=data)
+        self.client.force_authenticate(None)
+
+        expected_response_code = 400
+        expected_message = BAD_PASSWORD_PROVIDED
+        self.assertEqual(response.status_code, expected_response_code)
+        self.assertEqual(response.data.get('message'), expected_message)
+
+    def test_unmatching_password(self):
+        """Should be a bad request if two passwords do not match at /user/change-password/
+        """
+
+        data = {
+            'password': 'hello12345',
+            'repeat_password': 'hello12343'
+        }
+
+        # TODO: Use context manager
+        self.client.force_authenticate(self.user)
+        response = self.client.post(self.url, data=data)
+        self.client.force_authenticate(None)
+
+        expected_response_code = 400
+        expected_message = UNMATCHING_PASSWORDS
+        self.assertEqual(response.status_code, expected_response_code)
+        self.assertEqual(response.data.get('message'), expected_message)
+
+    def test_successful_password_change(self):
+        """Should change password successfully /user/change-password/
+        """
+        data = {
+            'password': 'hello12345',
+            'repeat_password': 'hello12345'
+        }
+
+        # TODO: Use context manager
+        self.client.force_authenticate(self.user)
+        response = self.client.post(self.url, data=data)
+        self.client.force_authenticate(None)
+
+        expected_response_code = 200
+        expected_message = RESET_PASSWORD_SUCCESS
+        self.assertEqual(response.status_code, expected_response_code)
+        self.assertEqual(response.data.get('message'), expected_message)
+
+        self.assertTrue(self.user.check_password(data['password']))
