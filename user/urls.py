bf8b4a92d67d57e75ac90325546cde2a743b747d
Saket Patel <Samy-33@users.noreply.github.com>
2019-08-05 22:19:27 +0530

    Added change password endpoint (#46)

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,8 @@ from .views import (
     UserRetrieveDeleteView,
     GetCurrentUserView,
     SendOTPView,
-    VerifyOTPView
+    VerifyOTPView,
+    ChangePasswordView
 )
 
 urlpatterns = [
@@ -12,5 +13,6 @@ urlpatterns = [
     path('me/', GetCurrentUserView.as_view(), name='get_current_user'),
     path('sign-up/send-otp/', SendOTPView.as_view(), name='send_otp_view'),
     path('sign-up/verify-otp/', VerifyOTPView.as_view(), name='verify_otp_view'),
+    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('<int:pk>/', UserRetrieveDeleteView.as_view(), name='get_and_delete')
 ]
