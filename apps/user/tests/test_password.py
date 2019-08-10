8d147531003b519b1d318d57323f1b95814d9ec8
Sachin Kukreja <skad5455@gmail.com>
2019-08-10 20:26:39 +0530

    Used mommy to make objects

--- apps/user/tests/test_password.py
+++ apps/user/tests/test_password.py
@@ -1,6 +1,7 @@
 from django.shortcuts import reverse
 from rest_framework.test import APITestCase
-from ..models import User
+from model_mommy import mommy
+from apps.globals.managers.test_managers import authenticated_user_api_client
 from ..views import (
     BAD_PASSWORD_PROVIDED,
     RESET_PASSWORD_SUCCESS,
@@ -10,12 +11,7 @@ from ..views import (
 class ChangePasswordViewTestCase(APITestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='username',
-            email='email@email.com',
-            password='random passwd'
-        )
-
+        self.user = mommy.make('user.User')
         self.url = reverse('change_password')
 
     def test_unauthenticated_request(self):
@@ -31,32 +27,25 @@ class ChangePasswordViewTestCase(APITestCase):
         """Should be a bad request if bad password provided \
         (not following strong password practices) at /user/change-password/
         """
-
         data = {'password': '123'}
+        with authenticated_user_api_client(self.client, self.user):
+            response = self.client.post(self.url, data=data)
 
-        # TODO: Use context manager
-        self.client.force_authenticate(self.user)
-        response = self.client.post(self.url, data=data)
-        self.client.force_authenticate(None)
-
-        expected_response_code = 400
-        expected_message = BAD_PASSWORD_PROVIDED
-        self.assertEqual(response.status_code, expected_response_code)
-        self.assertEqual(response.data.get('message'), expected_message)
+            expected_response_code = 400
+            expected_message = BAD_PASSWORD_PROVIDED
+            self.assertEqual(response.status_code, expected_response_code)
+            self.assertEqual(response.data.get('message'), expected_message)
 
     def test_successful_password_change(self):
         """Should change password successfully /user/change-password/
         """
         data = {'password': 'hello12345'}
+        with authenticated_user_api_client(self.client, self.user):
+            response = self.client.post(self.url, data=data)
 
-        # TODO: Use context manager
-        self.client.force_authenticate(self.user)
-        response = self.client.post(self.url, data=data)
-        self.client.force_authenticate(None)
+            expected_response_code = 200
+            expected_message = RESET_PASSWORD_SUCCESS
+            self.assertEqual(response.status_code, expected_response_code)
+            self.assertEqual(response.data.get('message'), expected_message)
 
-        expected_response_code = 200
-        expected_message = RESET_PASSWORD_SUCCESS
-        self.assertEqual(response.status_code, expected_response_code)
-        self.assertEqual(response.data.get('message'), expected_message)
-
-        self.assertTrue(self.user.check_password(data['password']))
+            self.assertTrue(self.user.check_password(data['password']))
