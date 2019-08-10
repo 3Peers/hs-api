99470b5b7fae85ee91207b93e2ae6b38b1eb1819
Sachin Kukreja <skad5455@gmail.com>
2019-08-10 20:04:56 +0530

    Added logging

--- apps/globals/utils/email.py
+++ apps/globals/utils/email.py
@@ -1,15 +1,18 @@
 from django.conf import settings
 from django.core.mail import send_mail as django_send_mail
 from celery import shared_task
+from celery.utils.log import get_task_logger
+
+logging = get_task_logger(__name__)
 
 
-# TODO: add logging on success/error of the sending email
 @shared_task
 def send_mail(subject, message, recipients):
     """
     Returns a boolean response to indicate status
     """
     if not (message and recipients):
+        logging.error("Unsent: Empty message and/or recipients.")
         return False
 
     num_emails_sent = django_send_mail(
@@ -18,5 +21,5 @@ def send_mail(subject, message, recipients):
         settings.EMAIL_HOST_USER,
         recipients
     )
-
+    logging.info(f"Sent: {num_emails_sent} emails.")
     return bool(num_emails_sent)
